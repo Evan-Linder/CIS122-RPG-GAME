837a9969136c14d6e6edf42c466d43c04367f3d8
Hieupham1701 <156444758+Hieupham1701@users.noreply.github.com>
2024-10-08 17:24:23 -0500

    DropDown , Debug Enemies
    Do the dropdown math script, enemies animator

---
+++ Assets/Script/Dropdown/Dropdown.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dropdown : MonoBehaviour
+{
+    public Dropdown dropbox;
+    public Text inputAnswer;
+    public string checkanswer;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void AddQuestion()
+    {
+        List<string> listquestion = new List<string>();
+        int num1 = Random.Range(0, 100);
+        int num2 = Random.Range(0, 100);
+    }
+}
