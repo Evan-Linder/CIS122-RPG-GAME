8d0909c15af2b1e01bd4ff04a0ef41508b01d45c
Evan Linder <109065222+Evan-Linder@users.noreply.github.com>
2024-10-16 23:24:56 -0500

    Bug fix with sound from last commit

--- Assets/Script/SoundEffects/SceneAudioPlayer.cs
+++ Assets/Script/SoundEffects/SceneAudioPlayer.cs
@@ -1,22 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SceneAudioPlayer : MonoBehaviour
 {
-    public AudioSource audioSource;  
-    public AudioClip sceneAudio;       
+    public AudioSource audioSource;
+    public AudioClip sceneAudio;
 
-    void Start()
+    void OnEnable() 
     {
         PlaySceneAudio();
     }
+
     void PlaySceneAudio()
     {
         if (audioSource != null && sceneAudio != null)
         {
+            if (audioSource.isPlaying)
+            {
+                audioSource.Stop();
+            }
+
+            
             audioSource.clip = sceneAudio;
             audioSource.Play();
+            Debug.Log("Playing Scene Audio");
+        }
+    }
+
+    void OnDisable() 
+    {
+        if (audioSource != null)
+        {
+            audioSource.Stop();
         }
     }
 }
+
+
