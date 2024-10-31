9891c721acd86612901636b3ea4bf3c2156c7627
Evan Linder <109065222+Evan-Linder@users.noreply.github.com>
2024-10-30 20:58:06 -0500

    Added a inventory panel

---
+++ Assets/Script/UI/InventoryToggle.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class InventoryToggle : MonoBehaviour
+{
+    public GameObject inventoryPanel;
+    private bool isInventoryOpen = false;
+
+    void Start()
+    { 
+        inventoryPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            ToggleInventory();
+        }
+    }
+
+    private void ToggleInventory()
+    {
+        isInventoryOpen = !isInventoryOpen;
+        inventoryPanel.SetActive(isInventoryOpen);
+    }
+}
+
