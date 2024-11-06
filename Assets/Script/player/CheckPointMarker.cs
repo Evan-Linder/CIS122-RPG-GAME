d98ec73aa05131cd690ef3d86746ba25c8e3ea86
Evan Linder <109065222+Evan-Linder@users.noreply.github.com>
2024-11-05 20:03:30 -0600

    Updates

--- Assets/Script/player/CheckPointMarker.cs
+++ Assets/Script/player/CheckPointMarker.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
+using TMPro;
 
 public class CheckPointMarker : MonoBehaviour
 {
-    public GameObject checkpointPrefab;  
-    public Transform scenesParent;      
-    private GameObject currentCheckpointMarker; 
-    private GameObject lastActiveScene; 
+    public GameObject checkpointPrefab;
+    public Transform scenesParent;
+    private GameObject currentCheckpointMarker;
+    private GameObject lastActiveScene;
     private GameObject currentActiveScene;
     public Animator sceneChangeAnim;
+
+    public TextMeshProUGUI locationMarkedText;
+    private float textDisplayTime = 2f;
 
     void Update()
     {
@@ -25,7 +29,17 @@ public class CheckPointMarker : MonoBehaviour
     // Marks the player's current position and saves the active scene
     void MarkCheckpoint()
     {
+        if (currentCheckpointMarker != null)
+        {
+            Destroy(currentCheckpointMarker);
+        }
+
         currentCheckpointMarker = Instantiate(checkpointPrefab, new Vector3(transform.position.x, transform.position.y - 1, transform.position.z), Quaternion.identity);
+
+        locationMarkedText.text = "Location Marked";
+        locationMarkedText.gameObject.SetActive(true);
+
+        Invoke("HideLocationMarkedPrompt", textDisplayTime);
 
         foreach (Transform scene in scenesParent)
         {
@@ -41,10 +55,11 @@ public class CheckPointMarker : MonoBehaviour
     // Teleports the player back to the marked position and switches the scenes
     void TeleportToCheckpoint()
     {
+
         sceneChangeAnim.Play("sceneChange");
-        
-        // Teleport the player to the marked position
         transform.position = currentCheckpointMarker.transform.position;
+
+        // Switch the scenes
         foreach (Transform scene in scenesParent)
         {
             if (scene.gameObject.activeSelf)
@@ -55,7 +70,13 @@ public class CheckPointMarker : MonoBehaviour
         }
         currentActiveScene.SetActive(false);
         lastActiveScene.SetActive(true);
+    }
 
-
+    // Hides the "Location Marked" prompt
+    void HideLocationMarkedPrompt()
+    {
+        locationMarkedText.gameObject.SetActive(false);
     }
 }
+
+
